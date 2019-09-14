c481ed0eee976a921839bfd83c5b40337c6cfdb0
cjogles <jackson.ogles28@gmail.com>
2019-09-14 14:03:14 -0600

    finished MVP for objects.js and challenges 1,2, and 3 for arrays.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,37 +10,96 @@
 // 5, adaine5@samsung.com, Antonietta, F
 
 // Example format of an intern object: 1, examples@you.edu, Example, F
-const example = {
-  id: 0,
-  name: "Example",
-  email: "examples@you.edu",
-  gender: "F",
-}
+// const example = {
+//   id: 0,
+//   name: "Example",
+//   email: "examples@you.edu",
+//   gender: "F",
+// }
 
 // Write your intern objects here:
 
+// Person Class (with constructor definition for new object initialization) 
+// for ECMA2015 or ES6 browser compatability, use class instead of function to initiate it.
+
+class Person {
+  constructor(id, name, email, gender) {
+    this.personID = id;
+    this.firstName = name;
+    this.personEmail = email;
+    this.sex = gender;
+  }
+
+  // getters and setters
+
+  // getter and setter for ID
+  get pID() {
+    return this.personID;
+  }
+  set pID(assignedID) {
+    this.personID = assignedID;
+  }
+  // getter and setter for name
+  get pName() {
+    return this.firstName;
+  }
+  set pName(assignedName) {
+    this.firstName = assignedName;
+  }
+  // getter and setter for email
+  get pEmail() {
+    return this.personEmail;
+  }
+  set pEmail(assignedEmail) {
+    this.personEmail = assignedEmail;
+  }
+  // getter and setter for gender
+  get pSex() {
+    return this.sex;
+  }
+  set pSex(assignedSex) {
+    this.sex = assignedSex;
+  }
+}
+
+person1 = new Person ("1", "Mitzi", "mmelloy0@psu.edu", "F");
+person2 = new Person ("2", "Kennan", "kdiben1@tinypic.com", "M");
+person3 = new Person ("3", "Keven", "kmummery2@wikimedia.org", "M");
+person4 = new Person ("4", "Gannie", "gmartinson3@illinois.edu", "M");
+person5 = new Person ("5", "Antonietta", "adaine5@samsung.com", "F");
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+//console.log(person1.pName);
 // Kennan's ID
-
+//console.log(person2.pID);
 // Keven's email
-
+//console.log(person3.pEmail);
 // Gannie's name
-
+//console.log(person4.pName);
 // Antonietta's Gender
+//console.log(person5.pSex);
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+person2.speak = function () {
+  return "Hello, my name is " + this.pName;
+}
+//console.log(person2.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
 
-// === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed the challenges in arrays.js and function-conversion.js.
+person5.multiplyNums = function (x, y) {
+  return x * y;
+}
+//console.log(person5.multiplyNums(2,6));
+
+// === Great work! === Head over to the the arrays.js. You may come back and attempt the Stretch Challenge once you have completed 
+// the challenges in arrays.js and function-conversion.js.
 
 // ==== Stretch Challenge: Nested Objects and the this keyword ==== 
 
